cad44e89f64ca5bc4401e9bb435f5ac8aa3b2b7c
葛志伟 <gezhiwei@jiangduoduo.com>
2018-07-17 14:58:00 +0800

    delete counter

--- src/main/java/com/jdddata/dockermgr/bussiness/service/ICounterService.java
+++
@@ -1,7 +0,0 @@
-package com.jdddata.dockermgr.bussiness.service;
-
-import com.jdddata.dockermgr.bussiness.controller.dto.CounterDto;
-
-public interface ICounterService {
-    String createCounter(CounterDto counterDto);
-}
