99d13e39c7952d495125d7d0a9899bf0b0186356
葛志伟 <gezhiwei@jiangduoduo>
2018-07-17 10:19:31 +0800

    init project with spring boot

---
+++ src/main/java/com/jdddata/dockermgr/DockerMgrApplication.java
@@ -0,0 +1,12 @@
+package com.jdddata.dockermgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerMgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerMgrApplication.class, args);
+	}
+}
