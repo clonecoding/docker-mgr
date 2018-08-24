9048dc5304ae607e048ac33f8c42d810f872e441
葛志伟 <gezhiwei@jiangduoduo.com>
2018-08-24 16:35:14 +0800

    指定日志

--- build/bin/start.bat
+++ build/bin/start.bat
@@ -6,6 +6,6 @@ set CONF_PATH=%base%\..\conf\
 set CLASSES_PATH=%CONF_PATH%;%LIBS_PATH%
 
 
-set JAVA_OPTS=-server -Xmx2g -Xms2g -Xmn1g -XX:+UseParallelGC -XX:ParallelGCThreads=4 -XX:+UseParallelOldGC -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -Xloggc:d:\dockermgr\gclogs\gc-%p.log
+set JAVA_OPTS=-server -Xmx2g -Xms2g -Xmn1g -XX:+UseParallelGC -XX:ParallelGCThreads=4 -XX:+UseParallelOldGC -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -Xloggc:E:\dockermgr\logs\gclogs\gc-%d{yyyy-MM-dd}.log.log
 java -Dfile.encoding=UTF-8 %JAVA_OPTS% -classpath %CLASSES_PATH% com.jdddata.dockermgr.DockerMgrApplication
 @pause
